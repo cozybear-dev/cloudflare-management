491096e925e3def47a997b9f3d5633b38dc8b987
cozybear-dev <7195866+cozybear-dev@users.noreply.github.com>
2025-07-02 16:53:35 +0200

    fix: disable redirect as domain is not in ownership anymore

--- main.tf
+++ main.tf
@@ -2,13 +2,13 @@ locals {
   account_id = "71ccf1d8c0f95f2629768483d5b41150"
 }
 
-module "domain_redirect" {
-  source     = "./modules/domain_redirect"
-  account_id = local.account_id
-  domain     = "shadowbrokers.hk"
-
-  redirect_target = "https://shadowbrokers.eu"
-}
+# module "domain_redirect" {
+#   source     = "./modules/domain_redirect"
+#   account_id = local.account_id
+#   domain     = "shadowbrokers.hk"
+#
+#   redirect_target = "https://shadowbrokers.eu"
+# }
 
 # this requires a web3 ipfs gateway which can only be setup manually!
 module "web3_hostname" {
